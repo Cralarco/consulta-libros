ede72609d7798ba9567187458a63c8b62b0bc208
Cralarco <calarconmilla@gmail.com>
2016-09-04 22:18:05 -0300

    Se agregan archivos iniciales
    test.py
    Autor: Alfredo
    test_Cris.py
    Autor: Cristian

---
+++ test.py
@@ -0,0 +1,24 @@
+﻿import urllib.request
+import json
+import os
+
+path = r'C:\Users\Ivette Escobar\Desktop\Python' # directory for search isbn.xml and save isbn json files.
+retval = os.getcwd()
+print ("Current working directory %s" % retval)
+os.chdir( path )
+retval = os.getcwd()
+print ("Directory changed successfully %s" % retval)
+
+isbn = open(r"isbn.xml", 'r')
+
+for link in isbn: # link is a line of isbn.xml.
+  response = urllib.request.urlopen("https://www.googleapis.com/books/v1/volumes?q=isbn:"+link)
+  data = response.read()
+  file = open(r'isbn'+link+'.json', 'wb') # file is a archive JSON of isbn information from API GOOGLE BOOKS.
+  file.write(data)
+  file.close
+  filejson=open(r'isbn'+link+'.json','r')
+  print("File isbn",link,".json write OK")
+  
+isbn.close()
+filejson.close()
---
+++ test_Cris.py
@@ -0,0 +1,44 @@
+﻿import urllib2
+import urllib
+import time
+import json
+import os
+from bs4 import BeautifulSoup as BS
+
+#
+#	URL para consultas: http://www.bookfinder.com/search/?keywords=9789876129978&new_used=&lang=&st=sh&ac=qr&submit=
+#
+#	ISBN de prueba
+#	isbn = "9781937965044"		# Scrum: a Breathtakingly Brief and Agile Introduction
+#	isbn = "9789876129091"		# Blackbird
+#	isbn = "9789876129978"		# Deadfall. Atrapada
+
+#	Lectura del archivo
+isbnFile = open( u'isbnFile.xml', 'r' )
+
+for isbn in isbnFile:
+	tmp = isbn.rstrip()
+	url = "http://www.bookfinder.com/search/?author=&title=&lang=es&isbn="+tmp+"&new_used=*&destination=es&currency=EUR&mode=basic&st=sr&ac=qr"
+	response = urllib2.urlopen(url)
+	html = response.read( )
+	response.close()
+	soup = BS( html, "html.parser" )
+	print soup.title.string
+	time.sleep( 1 )
+
+isbnFile.close()
+
+# isbn = "9789876129091"			# Deadfall. Atrapada
+# url = "http://www.bookfinder.com/search/?keywords="+isbn+"&new_used=&lang=&st=sh&ac=qr&submit="
+# url2 = "http://www.bookfinder.com/search/?author=&title=&lang=es&isbn="+isbn+"&new_used=*&destination=es&currency=EUR&mode=basic&st=sr&ac=qr"
+# response = urllib2.urlopen(url2)
+# html = response.read()
+# response.close()
+# soup = BS(html, "html.parser")
+# print soup.title.string
+#print html
+
+#http://www.bookfinder.com/search/?author=&title=&lang=es&isbn=9789876129978&new_used=*&destination=es&currency=EUR&mode=basic&st=sr&ac=qr
+
+
+#<form action="http://www.bookfinder.com/search/" method="GET" accept-charset="iso-8859-1" name="search_form" id="search_form" " class="search-form">
