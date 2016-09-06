bab88d5f1c5e64f221e94aeba6fe63dc0447e2b5
Cralarco <calarconmilla@gmail.com>
2016-09-06 07:40:48 -0300

    Se corrigio la codificacion de la web
    Existian problemas al leer las web por la codificación que esta tenia,
    por lo que se decodifico y volvio a codificar en utf-8

--- test_Cris.py
+++ test_Cris.py
@@ -1,5 +1,6 @@
 ﻿import urllib2
 import urllib
+import codecs
 import time
 import json
 import os
@@ -15,30 +16,20 @@ from bs4 import BeautifulSoup as BS
 
 #	Lectura del archivo
 isbnFile = open( u'isbnFile.xml', 'r' )
+resultadoFile = codecs.open('resultado', 'w', 'utf-8')
 
+print "Procesando el archivo isbnFile.xml"
 for isbn in isbnFile:
 	tmp = isbn.rstrip()
 	url = "http://www.bookfinder.com/search/?author=&title=&lang=es&isbn="+tmp+"&new_used=*&destination=es&currency=EUR&mode=basic&st=sr&ac=qr"
 	response = urllib2.urlopen(url)
-	html = response.read( )
+	html = response.read()
+	unicode_str = html.decode("iso-8859-1")
+	enconde_str = unicode_str.encode("utf8")
+	soup = BS( enconde_str, "html.parser" )
+	print soup.title.string.rstrip('\n')
 	response.close()
-	soup = BS( html, "html.parser" )
-	print soup.title.string
-	time.sleep( 1 )
-
+	resultadoFile.write(soup.title.string.rstrip('\n')+';'+tmp)
+	time.sleep( 10 )
+resultadoFile.close()
 isbnFile.close()
-
-# isbn = "9789876129091"			# Deadfall. Atrapada
-# url = "http://www.bookfinder.com/search/?keywords="+isbn+"&new_used=&lang=&st=sh&ac=qr&submit="
-# url2 = "http://www.bookfinder.com/search/?author=&title=&lang=es&isbn="+isbn+"&new_used=*&destination=es&currency=EUR&mode=basic&st=sr&ac=qr"
-# response = urllib2.urlopen(url2)
-# html = response.read()
-# response.close()
-# soup = BS(html, "html.parser")
-# print soup.title.string
-#print html
-
-#http://www.bookfinder.com/search/?author=&title=&lang=es&isbn=9789876129978&new_used=*&destination=es&currency=EUR&mode=basic&st=sr&ac=qr
-
-
-#<form action="http://www.bookfinder.com/search/" method="GET" accept-charset="iso-8859-1" name="search_form" id="search_form" " class="search-form">
